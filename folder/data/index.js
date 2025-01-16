69281f331f58c41cc29126745cef4d95080a0afe
victorfstedesco <victor.tedesco10@gmail.com>
2025-01-15 23:18:46 -0300

    corrections

--- folder/data/index.js
+++ folder/data/index.js
@@ -1,3 +1,32 @@
+const onload = function () {
+  const formulario = document.getElementById("h-campo-form");
+  
+  if (localStorage.getItem("formularioEnviado")) {
+    console.log("O formulário ja foi enviado por essa maquina");
+    formulario.innerHTML = `
+    <h2 class="bold">Inscrição já cadastrada! Obrigado.</h2>
+    `;
+  } else {
+    console.log("O formulário não foi enviado ainda");
+    formulario.innerHTML = `
+    <form action="https://sheetdb.io/api/v1/nxyqjiu1bz6ic" method="post" id="sheetdb-form">
+      <input type="name" name="Nome" placeholder="Seu nome" required maxlength="100" />
+      <input type="email" name="Email" placeholder="Seu melhor e-mail" required maxlength="100" />
+      <input type="telefone" maxlength="11" name="Telefone" placeholder="Telefone com DDD" required />
+      <input type="hidden" name="Created" value="x-sheetmonkey-current-date-time" />
+      <button type="submit" id="submitbutton">
+          <h6>Quero participar</h6>
+          <img src="/folder/assets/arrow-up.svg" alt="">
+      </button>
+  </form>
+   <p style = "font-size: 12px">Prometemos não usar nenhuma informação de contato para enviar qualquer tipo de SPAM. Os dados coletados são tratados nos termos da Lei Geral de Proteção de Dados e você pode se descadastrar da nossa lista a qualquer momento. Para mais informações, acesse nossa <a style="color: black; font-size: 12px;" href = "./politicaprivacidade.html" target="_blank">Política de Privacidade.</a></p>
+
+  `;
+  }
+};
+
+onload();
+
 var form = document.getElementById("sheetdb-form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -15,31 +44,5 @@ form.addEventListener("submit", (e) => {
     .then(localStorage.setItem("formularioEnviado", true));
 });
 
-
-const onload = function () {
-  const formulario = document.getElementById("h-campo-form");
-  
-  if (localStorage.getItem("formularioEnviado")) {
-    console.log("O formulário ja foi enviado por essa maquina");
-    formulario.innerHTML = `
-    <h2 class="bold">Inscrição já cadastrada! Obrigado.</h2>
-    `;
-  } else {
-    console.log("O formulário não foi enviado ainda");
-    formulario.innerHTML` 
-    <form action="https://sheetdb.io/api/v1/nxyqjiu1bz6ic" method="post" id="sheetdb-form">
-      <input type="name" name="Nome" placeholder="Seu nome" required />
-      <input type="email" name="Email" placeholder="Seu melhor e-mail" required />
-      <input type="telefone" name="Telefone" placeholder="Telefone com DDD" required />
-      <input type="hidden" name="Created" value="x-sheetmonkey-current-date-time" />
-      <button type="submit" id="submitbutton">
-          <h6>Quero participar</h6>
-          <img src="/folder/assets/arrow-up.svg" alt="">
-      </button>
-  </form>`;
-  }
-};
-
-onload();
 
 
